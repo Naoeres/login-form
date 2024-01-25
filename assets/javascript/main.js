5e34e6501910aaf464f68af1f581370d248bd705
Judson Silva <naoeeu2018@gmail.com>
2024-01-24 23:52:27 -0300

    almost finishing, we just have to delete the users

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -1,4 +1,5 @@
-const loginForm = document.querySelector("#login-form");
+const loginBtn = document.querySelector("#login-btn");
+
 const resetForm = document.querySelector("#reset-form");
 const passwordForm = document.querySelector("#password-form");
 const createForm = document.querySelector("#create-form");
@@ -17,10 +18,22 @@ const crePassword = document.querySelector("#cre-password");
 
 const message = document.querySelector("#message");
 
+const ulEl = document.querySelector('ul');
+const helloUser = document.querySelector('#hello-user');
+
+let id = 0;
+
 let emailFlag = "";
 let objList = [];
 let error = "";
 let success = "";
+
+let params = {
+    'firstName': '',
+    'lastName': ''
+};
+let emailLogin = '';
+let passwordLogin ='';
 
 /* Create a account area */
 const accountLink = document.querySelector('#new-account > a');
@@ -29,11 +42,13 @@ if(accountLink) {
         getUser()
         if(!objList) {
             objList = [{
+                'id': id,
                 'fName': 'admin',
                 'lName': 'admin',
-                'email': 'admin',
-                'password': 'admin'
+                'email': 'admin@email.com',
+                'password': '12345'
             }];
+            id++;
             setUser();
         }
     })
@@ -55,7 +70,7 @@ if (createForm) {
             return;
         }
 
-        createUser(fName.value, lName.value, email.value, crePassword.value);
+        createUser(id, fName.value, lName.value, email.value, crePassword.value);
 
         fName.value = "";
         lName.value = "";
@@ -96,6 +111,7 @@ if (passwordForm) {
             return;
         }
         
+        getUser();
         let newList = []
         objList.forEach(e => {
             if(e.email === emailFlag) e.password = fPassword.value;
@@ -130,15 +146,79 @@ if (resetForm) {
     });
 }
 
-function createUser(fName, lName, email, password) {
+/* --------------------------------------------------------------- */
+/* Login Area */
+
+if(loginBtn) {
+
+    loginBtn.addEventListener('click', e => {
+
+        if(!emailIndex.value || !password.value) {
+            e.preventDefault()
+            error = "all the fields should be filled!";
+            message.classList.add("error");
+            message.innerHTML = error;
+            return;
+        }
+
+        getUser();
+        objList.map(e => {
+            if(e.email === emailIndex.value && e.password === password.value) {
+                params.firstName = e.fName;
+                params.lastName = e.lName; 
+                emailLogin = e.email;
+                passwordLogin = e.password;
+            }
+        });
+
+        if(!emailLogin || !passwordLogin) {
+            e.preventDefault()
+            error = "wrong password or wrong email!";
+            message.classList.add("error");
+            message.innerHTML = error;
+            return;
+        }
+
+        localStorage.setItem('user', JSON.stringify(params));
+
+        document.location.href = './pages/users.html'
+    })
+}
+
+window.addEventListener('load', e => {
+    let nameF;
+    let nameL;
+
+    
+
+    params = JSON.parse(localStorage.getItem('user'));
+    if(params) {
+        nameF = params.firstName;
+        nameL = params.lastName
+    }
+
+
+    if(helloUser) {
+        helloUser.innerHTML = `Hello ${nameF} ${nameL}`;
+    }
+    
+    if(ulEl) {
+        createUserList(emailLogin, passwordLogin);
+    }
+})
+
+function createUser(id, fName, lName, email, password) {
     getUser();
 
     objList.push({
+        id,
         fName,
         lName,
         email,
         password,
     });
+
+    id++;
 
     setUser();
 }
@@ -149,4 +229,19 @@ function setUser() {
 
 function getUser() {
     objList = JSON.parse(localStorage.getItem("users"));
+}
+
+function createUserList(email, password) {
+    if(email === 'admin@email.com' && password === 'admin') {
+        getUser()
+        objList.forEach(e => {
+            ulEl.innerHTML += `<li> ${e.fName} ${e.lName} -> ${e.email} -> ${e.password} <button id="delete"> delete </button> </li>`;
+        })
+    }
+    else {
+        getUser()
+        objList.forEach(e => {
+            ulEl.innerHTML += `<li> ${e.fName} ${e.lName} -> ${e.email} -> ${e.password}</li>`;
+        })
+    }
 }
